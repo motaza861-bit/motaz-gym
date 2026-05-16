4088803155f58dee6ee058da8928f7a9b8b74d49
Motaz <adelmotaz861@gmail.com>
2026-05-16 03:44:56 +0300

    feat: wrap app in DateProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { DateProvider } from './context/DateContext'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode>
+    <DateProvider>
+      <App />
+    </DateProvider>
+  </StrictMode>
 )
